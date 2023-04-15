f3b39568a0957781cf9af18848a0e46840d0f63f
rangkotodotcom <softj4m98@gmail.com>
2023-04-16 01:29:08 +0700

    patient : progres migration

---
+++ database/migrations/2023_04_15_182129_create_patients_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Patients', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('rm_number');
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Patients');
+    }
+}

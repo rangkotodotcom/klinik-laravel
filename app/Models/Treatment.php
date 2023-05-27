c2961cf630f466e58b658bd944c052850a5b2ad8
rangkotodotcom <softj4m98@gmail.com>
2023-05-27 10:45:34 +0700

    set uuid

--- app/Models/Treatment.php
+++ app/Models/Treatment.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Traits\Uuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Treatment extends Model
 {
+    use Uuids;
     use HasFactory;
+
+
+    protected $guarded = [
+        'id'
+    ];
 }
